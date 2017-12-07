c0c4a612a5c7338b4d17f50767566d08a245c1d3
Talia Ringer <tringer@warfa.cs.washington.edu>
2017-12-07 15:06:44 -0800

    Move more to IOUtils

--- src/ioutils.mli
+++ src/ioutils.mli
@@ -9,3 +9,11 @@ val output_line : out_channel -> string -> unit
 (* Output a list of strings with newlines to an output channel *)
 val output_lines : out_channel -> string list -> unit
 
+(*
+ * Given an input file name, output file name, line number, and text,
+ * copy the text from input to output, writing text at the specified line.
+ *
+ * If the line number is greater than the number of lines in the file,
+ * then this writes at the end of the file.
+ *)
+val splice : string -> string -> int -> string list -> unit
