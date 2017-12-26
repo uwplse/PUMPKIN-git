7b839dd89f8a3307b809274055dc7767b0cf47b2
Talia Ringer <tringer@warfa.cs.washington.edu>
2017-12-26 13:01:34 -0800

    fix the dumbest error ever

--- src/changes/changes.ml
+++ src/changes/changes.ml
@@ -41,17 +41,14 @@ let changed_dependencies (filename : string) (id : string) (rev : string) =
     checkout rev;
     let g = dep_graph filename id in
     let cs_g = checksums g in
-    let cs_g' = cs_g in (* TODO temporary while we solve above *)
+    let cs_g' = cs_g in (* TODO temporary while we solve above problem~ *)
     checkout "-";
     stash_pop ();
     let cs = sub cs_g cs_g' in
     taint cs g
-  with Failure f -> (* TODO implement try/finally, catch all errors *)
-    try
-      checkout "-";
-      stash_pop ();
-      failwith (String.concat ": " ["Could not identify dependencies"; f])
-    with Failure f_git ->
-      stash_pop ();
-      failwith f_git
+  with Failure f -> (* TODO implement try/finally, catch errors separately, fix git state if necessary *)
+    checkout "-";
+    stash_pop ();
+    failwith (String.concat ": " ["Could not identify dependencies"; f])
+
 
